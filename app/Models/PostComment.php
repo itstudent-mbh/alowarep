4a425a46f8bfa8d73c64de76badad77482362f72
MohammadBagher Hanife <itstudent.mbh@gmail.com>
2022-05-06 13:06:28 +0430

    create model,migration and controller

---
+++ app/Models/PostComment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostComment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_name',
+        'comment',
+        'post_comment_id'
+    ];
+
+
+    public function parent(){
+        return $this->belongsTo(PostComment::class);
+    }
+
+    // get comment replies
+    public function childs(){
+        return $this->hasMany(PostComment::class);
+    }
+}
---
+++ database/migrations/2022_05_06_074059_create_post_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('post_id')->default(1);
+            $table->string('user_name',100);
+            $table->text('comment');
+            $table->foreignId('post_comment_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_comments');
+    }
+}
