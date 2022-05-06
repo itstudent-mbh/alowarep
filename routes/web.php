773b419c303c51c61790e2f27a64084bc9e8e9c1
MohammadBagher Hanife <itstudent.mbh@gmail.com>
2022-05-06 12:02:53 +0430

    create controller, view and route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PostController::class)->group(function () {
+    Route::get('/post/{id}','index')->name('post');
 });
